5e9d78fa4da30e31f8e811a3033ab6e8c2a4952d
Alejandro Irizarry <>
2023-04-17 02:07:54 +0000

    Added petition form and added dark mode to the section.

--- index.js
+++ index.js
@@ -8,9 +8,71 @@ const toggleDarkMode = () => {
   // Write your code to manipulate the DOM here
   document.body.classList.toggle("dark-mode");
   document.getElementById("home").classList.toggle("header-container-dark-mode");
+  document.getElementById("petition").classList.toggle("petition-container-dark-mode");
 }
 
 
 // TODO: Register a 'click' event listener for the theme button
 // Set toggleDarkMode as the callback function.
 themeButton.addEventListener("click", toggleDarkMode);
+
+
+
+// Add your query for the sign now button here
+const signNowButton = document.getElementById("sign-now-button")
+
+const addSignature = (event) => {
+  // Write your code to manipulate the DOM here
+  let name = document.getElementById("name").value;
+  let town = document.getElementById("town").value;
+  let email = document.getElementById("email").value;
+  let signatures = document.querySelector(".signatures");
+
+  const newP = document.createElement("p");
+  const newContent = document.createTextNode("🖊️ " + name + " from " + town + " supports this.");
+  newP.innerText = newContent.nodeValue;
+  signatures.appendChild(newP);
+
+}
+
+// Add a click event listener to the sign now button here
+//signNowButton.addEventListener("click", addSignature);
+
+
+
+
+// TODO: Remove the click event listener that calls addSignature()
+
+// TODO: Complete validation form
+
+const validateForm = () => {
+
+  let containsErrors = false;
+
+  var petitionInputs = document.getElementById("sign-petition").elements;
+
+  // TODO: Loop through all inputs
+  for (let i = 0; i < petitionInputs.length; i++) {
+    // TODO: Validate the value of each input
+    if (petitionInputs[i].value.length < 2) {
+      petitionInputs[i].classList.add('error');
+      containsErrors = true;
+    }
+    else {
+      petitionInputs[i].classList.remove('error');
+    }
+  }
+  
+  if(containsErrors == false) {
+    // TODO: Call addSignature() and clear fields if no errors
+    addSignature();
+    for (let i = 0; i < petitionInputs.length; i++) {
+      petitionInputs[i].value = "";
+      containsErrors = false;
+    }
+  }
+
+
+}
+
+signNowButton.addEventListener('click', validateForm);
